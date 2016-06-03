a473e088bdc2a0b7972b304a284e4786a6cb8a44
Karolien <kkoo939@aucklanduni.ac.nz>
2016-06-03 21:50:11 +1200

    Added plant maturity feature

--- Lily.java
+++ Lily.java
@@ -4,17 +4,20 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JTextArea;
 
 public class Lily extends Flower implements Runnable{
 	private int width;
 	private int height;
+	private JTextArea output;
 
 
-	public Lily(int x, int y){
+	public Lily(int x, int y, JTextArea outputArea){
 		super(x,y);
-		width = 40;
-		height = 60;
-		type = "Lily";
+		width = 20;
+		height = 30;
+		type = "lily seedling";
+		output = outputArea;
 	}
 	@Override
 	public void run() {
@@ -28,7 +31,7 @@ public class Lily extends Flower implements Runnable{
 			
 		}
 		finally{
-			System.out.println("Your " + type + " died!");
+			output.append("Your " + type + " died!\n");
 			alive = false;
 		}
 	}
@@ -36,13 +39,36 @@ public class Lily extends Flower implements Runnable{
 		return (p.x <= mousePt.x && mousePt.x <= (p.x + width + 1)	&&	p.y <= mousePt.y && mousePt.y <= (p.y + height + 1));
 	}
 
-	
+	public void water(){
+		waterLevel += 50;
+		if(waterLevel > 150){
+			alive = false;
+		}
+		careLevel++;
+		if(careLevel==10){
+			maturity++;
+			width = 40;
+			height = 60;
+			output.append("Your " + type + " matured!\n");
+			type = "lily";
+		}
+	}
 	public void draw(Graphics g) {
-		BufferedImage audi = null;
-		try {
-			audi = ImageIO.read(GUI.class.getResource("lily.png"));
-		} catch (IOException e) {}
-		g.drawImage(audi, p.x, p.y, width, height, null);
+		BufferedImage lily = null;
+		switch(maturity){
+			case 0:
+				try {
+					lily = ImageIO.read(GUI.class.getResource("seedling.png"));
+				} catch (IOException e) {}
+				g.drawImage(lily, p.x, p.y, width, height, null);
+				break;
+			case 1:
+				try {
+					lily = ImageIO.read(GUI.class.getResource("lily.png"));
+				} catch (IOException e) {}
+				g.drawImage(lily, p.x, p.y, width, height, null);
+				break;
+		}
 		g.drawString(Integer.toString(waterLevel), p.x, p.y);
 	}
 	
--- Rose.java
+++ Rose.java
@@ -1,21 +1,23 @@
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JTextArea;
 
 public class Rose extends Flower implements Runnable{
 	private int width;
 	private int height;
+	private JTextArea output;
 
 
-	public Rose(int x, int y){
+	public Rose(int x, int y, JTextArea outputArea){
 		super(x,y);
-		width = 40;
-		height = 60;
-		type = "Rose";
+		width = 20;
+		height = 30;
+		type = "rose seedling";
+		output = outputArea;
 	}
 	@Override
 	public void run() {
@@ -29,21 +31,45 @@ public class Rose extends Flower implements Runnable{
 			
 		}
 		finally{
-			System.out.println("Your " + type + " died!");
+			output.append("Your " + type + " died!\n");
 			alive = false;
 		}
 	}
 	public boolean contains(Point mousePt) {
 		return (p.x <= mousePt.x && mousePt.x <= (p.x + width + 1)	&&	p.y <= mousePt.y && mousePt.y <= (p.y + height + 1));
 	}
-
+	
+	public void water(){
+		waterLevel += 50;
+		if(waterLevel > 150){
+			alive = false;
+		}
+		careLevel++;
+		if(careLevel==10){
+			maturity++;
+			width = 40;
+			height = 60;
+			output.append("Your " + type + " matured!\n");
+			type = "rose";
+		}
+	}
 	
 	public void draw(Graphics g) {
-		BufferedImage audi = null;
-		try {
-			audi = ImageIO.read(GUI.class.getResource("rose.png"));
-		} catch (IOException e) {}
-		g.drawImage(audi, p.x, p.y, width, height, null);
+		BufferedImage rose = null;
+		switch(maturity){
+			case 0:
+				try {
+					rose = ImageIO.read(GUI.class.getResource("seedling.png"));
+				} catch (IOException e) {}
+				g.drawImage(rose, p.x, p.y, width, height, null);
+				break;
+			case 1:
+				try {
+					rose = ImageIO.read(GUI.class.getResource("rose.png"));
+				} catch (IOException e) {}
+				g.drawImage(rose, p.x, p.y, width, height, null);
+				break;
+		}
 		g.drawString(Integer.toString(waterLevel), p.x, p.y);
 	}
 	
