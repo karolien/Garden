db1561ed4a7fb782a83091e11ef5348d5e834395
Karolien <kkoo939@aucklanduni.ac.nz>
2016-06-03 19:18:29 +1200

    Changed flower to abstract class

--- Flower.java
+++ Flower.java
@@ -2,54 +2,36 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 
-public class Flower implements Runnable{
-	private String type;
-	private int waterLevel;
-	private int maturity;
-	private Point p;
-	private int width;
-	private int height;
+public abstract class Flower implements Runnable{
+	protected String type;
+	protected int waterLevel;
+	protected int maturity;
+	protected Point p;
+	protected boolean alive = true;
 
-	public Flower(String type, int x, int y){
-		this.type = type;
+	public Flower(int x, int y){
 		waterLevel = 50;
 		maturity = 0;
 		p = new Point(x,y);
-		width = 20;
-		height = 20;
 	}
 	@Override
-	public void run() {
-		try{
-			while(waterLevel > 0){
-				Thread.sleep(1000);
-				waterLevel--;
-			}
-		}
-		catch(Exception e){
-			
-		}
-		finally{
-			System.out.println("Your " + type + " died!");
-		}
-	}
-	public boolean contains(Point mousePt) {
-		return (p.x <= mousePt.x && mousePt.x <= (p.x + width + 1)	&&	p.y <= mousePt.y && mousePt.y <= (p.y + height + 1));
-	}
+	public abstract void run();
+	public abstract boolean contains(Point mousePt);
 	
 	public void water(){
-		waterLevel = 100;
+		waterLevel += 50;
+		if(waterLevel > 150){
+			alive = false;
+		}
+	}
+	public boolean isAlive(){
+		return alive;
 	}
 	
 	public String getType(){
 		return type;
 	}
 	
-	public void draw(Graphics g) {	//very ugly flower at the moment!
-		g.setColor(Color.blue);
-		g.fillRect(p.x, p.y, width, height);
-		g.setColor(Color.black);
-		g.drawRect(p.x, p.y, width, height);
-	}
+	public abstract void draw(Graphics g);
 	
 }
